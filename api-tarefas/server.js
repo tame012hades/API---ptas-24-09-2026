c1b73726be263f21bfe93368faae30825091980b
João <joaovitorrochacardosodemenez@gmail.com>
2026-09-23 18:23:12 +0000

    retornando status 201

--- api-tarefas/server.js
+++ api-tarefas/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const tarefas = [
     {
         id: 1,
@@ -50,6 +52,19 @@ app.get('/tarefas/:id', (req, res) => {
     }
 
     res.json(tarefa);
+});
+
+
+app.post('/tarefas', (req, res) => {
+    const novaTarefa = {
+        id: tarefas.length + 1,
+        titulo: req.body.titulo,
+        concluida: false
+    };
+
+    tarefas.push(novaTarefa);
+
+    res.status(201).json(novaTarefa);
 });
 
 app.listen(3000, () => {
