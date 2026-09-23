bbe26789d21eefe75f60325f3a653bfb44b67fdf
João <joaovitorrochacardosodemenez@gmail.com>
2026-09-23 18:12:21 +0000

    erro e mensagem implementado

--- api-tarefas/server.js
+++ api-tarefas/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const app = express();
 
-
 const tarefas = [
     {
         id: 1,
@@ -26,9 +25,23 @@ app.get('/', (req, res) => {
     res.send('API de Tarefas no ar');
 });
 
-
 app.get('/tarefas', (req, res) => {
     res.json(tarefas);
+});
+
+
+app.get('/tarefas/:id', (req, res) => {
+    const id = Number(req.params.id);
+
+    const tarefa = tarefas.find(t => t.id === id);
+
+    if (!tarefa) {
+        return res.status(404).json({
+            erro: 'Tarefa não encontrada'
+        });
+    }
+
+    res.json(tarefa);
 });
 
 app.listen(3000, () => {
