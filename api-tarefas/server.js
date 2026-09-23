efd32c64e55e8d4aff4b4a4420809e41894b05d2
João <joaovitorrochacardosodemenez@gmail.com>
2026-09-23 18:04:47 +0000

    3 tarefas implementadas

--- api-tarefas/server.js
+++ api-tarefas/server.js
@@ -2,8 +2,35 @@ const express = require('express');
 
 const app = express();
 
+
+const tarefas = [
+    {
+        id: 1,
+        titulo: 'Estudar',
+        concluida: false
+    },
+    {
+        id: 2,
+        titulo: 'Fazer trabalho',
+        concluida: true
+    },
+    {
+        id: 3,
+        titulo: 'Ir para a escola',
+        concluida: false
+    }
+];
+
+
 app.get('/', (req, res) => {
-  res.send('API de Tarefas no ar');
+    res.send('API de Tarefas no ar');
 });
 
-app.listen(3000);
+
+app.get('/tarefas', (req, res) => {
+    res.json(tarefas);
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
